bac702528d3fef0fcb315739dc08da36f6881259
Satyateja Pothuru <satyateja27@gmail.com>
2017-10-14 22:19:34 -0700

    Login Module Skeleton

---
+++ app/src/main/java/edu/sjsu/intentrecognitionchatapplication/LoginActivity.java
@@ -0,0 +1,19 @@
+package edu.sjsu.intentrecognitionchatapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by satya on 10/14/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
