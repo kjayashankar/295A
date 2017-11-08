c64a04503fb985d3e7251f911ec2a81412b3dc85
kjayashankar <GitHub!@34>
2017-11-08 02:32:12 -0800

    websockets friends list

--- app/src/main/java/edu/sjsu/intentrecognitionchatapplication/ChatFriendsActivity.java
+++ app/src/main/java/edu/sjsu/intentrecognitionchatapplication/ChatFriendsActivity.java
@@ -3,21 +3,32 @@ package edu.sjsu.intentrecognitionchatapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import edu.sjsu.intentrecognitionchatapplication.adapter.ManageChatFriendsAdapter;
+import edu.sjsu.intentrecognitionchatapplication.data.ChatMessage;
 import edu.sjsu.intentrecognitionchatapplication.data.Friend;
+import edu.sjsu.intentrecognitionchatapplication.websockets.WebSocketClient;
 
 public class ChatFriendsActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private List<Friend> mFriendList = null;
     private ListView listActiveFriends;
+    private String myName = "Jayashankar Karnam";
+    private WebSocketClient client = null;
+    private static final String TAG = "ChatFriendsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,23 +48,91 @@ public class ChatFriendsActivity extends AppCompatActivity implements AdapterVie
                 //startActivity(i);
             }
         });
-        getActiveFriends();
+        listActiveFriends.setOnItemClickListener(this);
+        setNotificationSocket();
     }
 
     private void getActiveFriends(){
-        mFriendList = new ArrayList<Friend>();
 
-        mFriendList.add(new Friend("Chat Friend1"));
-        mFriendList.add(new Friend("Chat Friend2"));
-        mFriendList.add(new Friend("Chat Friend3"));
-        mFriendList.add(new Friend("Chat Friend4"));
-        mFriendList.add(new Friend("Chat Friend5"));
-
-        ManageChatFriendsAdapter adapter = new ManageChatFriendsAdapter(this, R.layout.chat_list_item, mFriendList);
-        listActiveFriends.setAdapter(adapter);
-        listActiveFriends.setOnItemClickListener(this);
     }
 
+    private void setNotificationSocket(){
+        String path = "http://10.0.0.98:8080/IntentChatServer/notification?from="+myName;
+
+        Log.d(TAG,path);
+        client = new WebSocketClient(URI.create(path.replaceAll(" ","+")), new WebSocketClient.Listener() {
+        @Override
+        public void onConnect() {
+            Log.d("TAG", "connection opened");
+        }
+
+        /**
+         * On receiving the message from web socket server
+         * */
+        @Override
+        public void onMessage(final String message) {
+            Log.d("TAG", String.format("Got string message! %s", message));
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mFriendList = new ArrayList<Friend>();
+
+                    JSONArray array = null;
+                    try {
+                        array = new JSONArray(message);
+                    }
+                    catch(Exception e){
+
+                    }
+                    if (array == null){
+                        listActiveFriends.setAdapter(null);
+                        return;
+                    }
+                    int size = array.length();
+
+                    while (size-- > 0){
+                        JSONObject object;
+                        try {
+                            object = array.getJSONObject(size);
+                            mFriendList.add(new Friend(object.getString("name")));
+                        }
+                        catch(Exception e){}
+                    }
+                    ManageChatFriendsAdapter adapter = new ManageChatFriendsAdapter(getApplicationContext(), R.layout.chat_list_item, mFriendList);
+                    listActiveFriends.setAdapter(adapter);
+                }
+            });
+        }
+
+        @Override
+        public void onMessage(byte[] data) {
+            Log.d("TAG", String.format("Got binary message! %s"
+            ));
+        }
+
+        /**
+         * Called when the connection is terminated
+         * */
+        @Override
+        public void onDisconnect(int code, String reason) {
+
+            String message = String.format(Locale.US,
+                    "Disconnected! Code: %d Reason: %s", code, reason);
+        }
+
+        @Override
+        public void onError(Exception error) {
+            Log.e("TAG", "Error! : " + error.getMessage());
+            Log.e("TAG","err",error.fillInStackTrace());
+
+            // showToast("Error! : " + error);
+        }
+
+    }, null);
+
+        client.connect();
+}
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
