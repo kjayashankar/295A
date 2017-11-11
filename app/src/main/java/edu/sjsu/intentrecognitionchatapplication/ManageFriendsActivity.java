f1cfdb282f0e663651c7367aaacadb5e032d510e
kjayashankar <GitHub!@34>
2017-11-10 23:05:50 -0800

    manage friends page v2

--- app/src/main/java/edu/sjsu/intentrecognitionchatapplication/ManageFriendsActivity.java
+++ app/src/main/java/edu/sjsu/intentrecognitionchatapplication/ManageFriendsActivity.java
@@ -1,11 +1,20 @@
 package edu.sjsu.intentrecognitionchatapplication;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,8 +23,10 @@ import edu.sjsu.intentrecognitionchatapplication.data.Friend;
 
 public class ManageFriendsActivity extends AppCompatActivity {
 
+    private static String END_POINT_URL="http://10.0.0.98:8080/IntentChatServer/service/friendsPage/";
     List<Friend> friends;
     String method = "";
+    private static String myName = "Jayashankar+Karnam";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +43,9 @@ public class ManageFriendsActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
+        StrictMode.setThreadPolicy(policy);
         initButtons();
 
 
@@ -42,6 +55,7 @@ public class ManageFriendsActivity extends AppCompatActivity {
         friends = new ArrayList<Friend>();
         final TextView friendVanilla = (TextView) findViewById(R.id.friendVanilla);
         final TextView friendRequests = (TextView) findViewById(R.id.friendRequests);
+        final TextView friendConfirmations = (TextView) findViewById(R.id.friendConfirmations);
 
         friendVanilla.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,8 +63,8 @@ public class ManageFriendsActivity extends AppCompatActivity {
                 method = "friendVanilla";
                 friendVanilla.setBackgroundColor(getResources().getColor(R.color.fillhighlight));
                 friendRequests.setBackgroundColor(getResources().getColor(R.color.fill));
-
-                populateList("friend");
+                friendConfirmations.setBackgroundColor(getResources().getColor(R.color.fill));
+                populateList("friends");
                 inflateUI();
                 //new SearchFriends("friend").execute();
             }
@@ -61,8 +75,22 @@ public class ManageFriendsActivity extends AppCompatActivity {
                 method = "friendRequests";
                 friendVanilla.setBackgroundColor(getResources().getColor(R.color.fill));
                 friendRequests.setBackgroundColor(getResources().getColor(R.color.fillhighlight));
+                friendConfirmations.setBackgroundColor(getResources().getColor(R.color.fill));
 
                 populateList("requests");
+                inflateUI();
+                //new SearchFriends("requests").execute();
+            }
+        });
+        friendConfirmations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                method = "friendRequests";
+                friendVanilla.setBackgroundColor(getResources().getColor(R.color.fill));
+                friendRequests.setBackgroundColor(getResources().getColor(R.color.fill));
+                friendConfirmations.setBackgroundColor(getResources().getColor(R.color.fillhighlight));
+
+                populateList("confirmations");
                 inflateUI();
                 //new SearchFriends("requests").execute();
             }
@@ -87,19 +115,107 @@ public class ManageFriendsActivity extends AppCompatActivity {
     }
     private void populateList(String category) {
         switch(category){
+            case "friends":
+                friends = new ArrayList<Friend>();
+                runOnUiThread(new Runnable(){
+                    @Override
+                    public void run() {
+                        friends = new ArrayList<Friend>();
+                        JSONArray array = null;
+                        StringBuilder result = new StringBuilder();
+                        try {
+                            URL url = new URL(END_POINT_URL + "friends/"+myName);
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                            conn.setRequestMethod("GET");
+                            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                            String line;
+                            while ((line = rd.readLine()) != null) {
+                                result.append(line);
+                            }
+                            rd.close();
+                            array = new JSONArray(result.toString());
+                            int size = array.length();
+                            int i = 0;
+                            while(i < size ) {
+                                JSONObject obj = array.getJSONObject(i);
+                                friends.add(new Friend(obj.getString("name")));
+                                i++;
+                            }
+                        }
+                        catch(Exception e){
+                            Log.e("ManageFriends","fetch downstream data",e.fillInStackTrace());
+                        }
+                        Log.v("ManageFriends",result.toString());
+                    }
+                });
+                break;
             case "requests":
                 friends = new ArrayList<Friend>();
-                friends.add(new Friend("Request1"));
-                friends.add(new Friend("Request2"));
-                friends.add(new Friend("Request3"));
-                friends.add(new Friend("Request4"));
+                runOnUiThread(new Runnable(){
+                    @Override
+                    public void run() {
+                        friends = new ArrayList<Friend>();
+                        JSONArray array = null;
+                        StringBuilder result = new StringBuilder();
+                        try {
+                            URL url = new URL(END_POINT_URL + "requests/"+myName);
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                            conn.setRequestMethod("GET");
+                            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                            String line;
+                            while ((line = rd.readLine()) != null) {
+                                result.append(line);
+                            }
+                            rd.close();
+                            array = new JSONArray(result.toString());
+                            int size = array.length();
+                            int i = 0;
+                            while(i < size ) {
+                                JSONObject obj = array.getJSONObject(i);
+                                friends.add(new Friend(obj.getString("name")));
+                                i++;
+                            }
+                        }
+                        catch(Exception e){
+                            Log.e("ManageFriends","fetch downstream data",e.fillInStackTrace());
+                        }
+                        Log.v("ManageFriends",result.toString());
+                    }
+                });
                 break;
-            case "friend":
+            case "confirmations":
                 friends = new ArrayList<Friend>();
-                friends.add(new Friend("Friend1"));
-                friends.add(new Friend("Friend2"));
-                friends.add(new Friend("Friend3"));
-                friends.add(new Friend("Friend4"));
+                runOnUiThread(new Runnable(){
+                    @Override
+                    public void run() {
+                        friends = new ArrayList<Friend>();
+                        JSONArray array = null;
+                        StringBuilder result = new StringBuilder();
+                        try {
+                            URL url = new URL(END_POINT_URL + "confirmations/"+myName);
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                            conn.setRequestMethod("GET");
+                            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                            String line;
+                            while ((line = rd.readLine()) != null) {
+                                result.append(line);
+                            }
+                            rd.close();
+                            array = new JSONArray(result.toString());
+                            int size = array.length();
+                            int i = 0;
+                            while(i < size ) {
+                                JSONObject obj = array.getJSONObject(i);
+                                friends.add(new Friend(obj.getString("name")));
+                                i++;
+                            }
+                        }
+                        catch(Exception e){
+                            Log.e("ManageFriends","fetch downstream data",e.fillInStackTrace());
+                        }
+                        Log.v("ManageFriends",result.toString());
+                    }
+                });
                 break;
             default:
                 break;
